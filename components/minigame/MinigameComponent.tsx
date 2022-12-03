ea9b06c9b0927eec8f8bdc4165de4286ef77840f
romain22222 <romrom22222@hotmail.fr>
2022-12-03 14:58:33 +0100

    fix

--- components/minigame/MinigameComponent.tsx
+++ components/minigame/MinigameComponent.tsx
@@ -1,69 +1,53 @@
-import {
-  CaseTemplate,
-  GridMinigame,
-  IMinigame,
-} from "../../model/Minigames/IMinigame";
-import { Box, Button } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import { IEvent } from "../../model/Events/IEvent";
-import { MinigameController } from "../../model/Minigames/MinigameController";
+import {CaseTemplate, GridMinigame, IMinigame,} from "../../model/Minigames/IMinigame";
+import {Box, Button} from "@chakra-ui/react";
+import {useEffect, useState} from "react";
+import {MinigameController} from "../../model/Minigames/MinigameController";
 
 // TODO quand le jeu est terminé, se remettre sur le prochain event
 
 export default function MinigameComponent() {
-  let logicGrid: GridMinigame = new Map();
-  let miniGameId: any;
+    let logicGrid: GridMinigame = new Map();
+    let miniGameId: string;
 
-  let minigame!: IMinigame;
-  let minigameName: string;
+    let minigame!: IMinigame;
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            miniGameId = localStorage.getItem("miniGameId") as string;
+            minigame = miniGameId === "random" ? MinigameController.pickRandomMinigame() : MinigameController.getById(miniGameId);
+            let rowTemplate: CaseTemplate[] = [];
+            for (let i = 0; i < minigame.nbRow; i++) {
+                rowTemplate.push(minigame.caseTemplateCreate());
+            }
+            for (let i = 0; i < minigame.nbCol; i++) {
+                logicGrid.set(
+                    String.fromCharCode(65 + i),
+                    JSON.parse(JSON.stringify(rowTemplate))
+                );
+            }
+            return minigame.init(logicGrid, setUpdateGrid, setLogicGrid);
+        }
+    }, []);
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      miniGameId = localStorage.getItem("miniGameId");
-      minigame = miniGameId
-        ? MinigameController.getById(miniGameId)
-        : MinigameController.pickRandomMinigame();
-      console.log(miniGameId, "miniGameId");
-      console.log(
-        MinigameController.getById(miniGameId as string),
-        "minigameFromController"
-      );
-      console.log("minigame", minigame);
-
-      let rowTemplate: CaseTemplate[] = [];
-      for (let i = 0; i < minigame.nbRow; i++) {
-        rowTemplate.push(minigame.caseTemplateCreate());
-      }
-      for (let i = 0; i < minigame.nbCol; i++) {
-        logicGrid.set(
-          String.fromCharCode(65 + i),
-          JSON.parse(JSON.stringify(rowTemplate))
-        );
-      }
-      return minigame.init(logicGrid, setUpdateGrid, setLogicGrid);
-    }
-  }, []);
-
-  let [updateGrid, setUpdateGrid] = useState(false);
-  let [logicGridToUpdate, setLogicGrid] = useState(logicGrid);
-  useEffect(() => {
-    if (updateGrid) {
-      minigame.update(logicGridToUpdate);
-      setUpdateGrid(false);
-    }
-  }, [updateGrid]);
-  return (
-    <>
-      {minigame ? (
-        <Box id="Canvas">
-          <Box id="Grid">{minigame.ViewGrid(logicGridToUpdate)}</Box>
-          <Button
-            onClick={() => minigame.playerInput("test", logicGridToUpdate)}
-          />
-        </Box>
-      ) : (
-        <></>
-      )}
-    </>
-  );
+    let [updateGrid, setUpdateGrid] = useState(false);
+    let [logicGridToUpdate, setLogicGrid] = useState(logicGrid);
+    useEffect(() => {
+        if (updateGrid) {
+            minigame.update(logicGridToUpdate);
+            setUpdateGrid(false);
+        }
+    }, [updateGrid]);
+    return (
+        <>
+            {minigame ? (
+                <Box id="Canvas">
+                    <Box id="Grid">{minigame.ViewGrid(logicGridToUpdate)}</Box>
+                    <Button
+                        onClick={() => minigame.playerInput("test", logicGridToUpdate)}
+                    />
+                </Box>
+            ) : (
+                <></>
+            )}
+        </>
+    );
 }
