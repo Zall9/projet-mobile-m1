ded11f9d521ca3085f12fcd6b1cf28cf151c2317
Paul Delifer <paul.delifer@yahoo.fr>
2022-12-05 12:13:18 +0100

    s

--- model/Events/EventList/ForestTent.ts
+++ model/Events/EventList/ForestTent.ts
@@ -1,16 +1,16 @@
-import {IEvent} from "../IEvent";
-import {Direction} from "../IOutput";
+import { IEvent } from "../IEvent";
+import { Direction } from "../IOutput";
 
 export const eventInfos: IEvent = {
-    nom: "Aire de repos d'Yggdrisal",
-    description: "Vous vous reposez dans l'aire de repos d'Yggdrisal.",
-    image: "tent_forest",
-    sorties: [
-        {
-            direction: Direction.up,
-            nextEvent: "StartingEventMage",
-            willMinigame: true,
-            whichMinigame: "Panier"
-        }
-    ]
-}
+  nom: "Aire de repos d'Yggdrisal",
+  description: "Vous vous reposez dans l'aire de repos d'Yggdrisal.",
+  image: "tent_forest",
+  sorties: [
+    {
+      direction: Direction.up,
+      nextEvent: "StartingEventMage",
+      willMinigame: true,
+      whichMinigame: "Memory",
+    },
+  ],
+};
