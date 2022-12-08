4dd23febf2fd12257d5f5841300de7216e9ea740
romain22222 <romrom22222@hotmail.fr>
2022-12-08 23:30:26 +0100

    small updates

--- components/minigame/MinigameComponent.tsx
+++ components/minigame/MinigameComponent.tsx
@@ -1,101 +1,81 @@
-import {
-  CaseTemplate,
-  GridMinigame,
-  IMinigame,
-} from "../../model/Minigames/IMinigame";
-import { Box, IconButton } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import { MinigameController } from "../../model/Minigames/MinigameController";
-import { minigameInfos } from "../../model/Minigames/MinigameList/Unknown";
-import { useRouter } from "next/router";
-import { ArrowLeftIcon, ArrowRightIcon } from "@chakra-ui/icons";
+import {CaseTemplate, GridMinigame, IMinigame,} from "../../model/Minigames/IMinigame";
+import {Box} from "@chakra-ui/react";
+import {useEffect, useState} from "react";
+import {MinigameController} from "../../model/Minigames/MinigameController";
+import {minigameInfos} from "../../model/Minigames/MinigameList/Unknown";
+import {useRouter} from "next/router";
 
 export default function MinigameComponent() {
-  let router = useRouter();
-  let logicGrid: GridMinigame = new Map();
-  let miniGameId: string;
+    let router = useRouter();
+    let logicGrid: GridMinigame = new Map();
+    let miniGameId: string;
 
-  let [minigame, setMinigame] = useState<IMinigame>(minigameInfos);
-  let [reset, sr] = useState(false);
-  useEffect(() => {
-    miniGameId = localStorage.getItem("miniGameId") as string;
-    setMinigame(
-      miniGameId === "random"
-        ? MinigameController.pickRandomMinigame()
-        : MinigameController.getById(miniGameId)
-    );
-    if (minigame === minigameInfos) {
-      sr(!reset);
-      return;
-    }
-    let rowTemplate: CaseTemplate[] = [];
-    for (let i = 0; i < minigame.nbRow; i++) {
-      rowTemplate.push(minigame.caseTemplateCreate());
-    }
-    for (let i = 0; i < minigame.nbCol; i++) {
-      logicGrid.set(
-        String.fromCharCode(65 + i),
-        JSON.parse(JSON.stringify(rowTemplate))
-      );
-    }
-    minigame.score = 0;
-    minigame.init(logicGrid, setUpdateGrid, setLogicGrid);
-    setLogicGrid(logicGrid);
-    setUpdateGrid(true);
-  }, [reset]);
-
-  let [updateGrid, setUpdateGrid] = useState(false);
-  let [logicGridToUpdate, setLogicGrid] = useState(logicGrid);
-  //@ts-ignore
-  useEffect(() => {
-    //@ts-ignore
-    const myInterval = setInterval(() => {
-      minigame.evolve(logicGridToUpdate);
-    }, minigame.refreshInterval);
-    if (updateGrid) {
-      if (minigame.score >= minigame.scoreTresh) {
-        localStorage.setItem(
-          "score",
-          (
-            parseInt(localStorage.getItem("score") as string) + minigame.score
-          ).toString()
+    let [minigame, setMinigame] = useState<IMinigame>(minigameInfos);
+    let [reset, sr] = useState(false);
+    useEffect(() => {
+        miniGameId = localStorage.getItem("miniGameId") as string;
+        setMinigame(
+            miniGameId === "random"
+                ? MinigameController.pickRandomMinigame()
+                : MinigameController.getById(miniGameId)
         );
-        router.push("/game").then(() => null);
-      }
-      minigame.update(logicGridToUpdate);
-      setUpdateGrid(false);
-    }
+        if (minigame === minigameInfos) {
+            sr(!reset);
+            return;
+        }
+        let rowTemplate: CaseTemplate[] = [];
+        for (let i = 0; i < minigame.nbRow; i++) {
+            rowTemplate.push(minigame.caseTemplateCreate());
+        }
+        for (let i = 0; i < minigame.nbCol; i++) {
+            logicGrid.set(
+                String.fromCharCode(65 + i),
+                JSON.parse(JSON.stringify(rowTemplate))
+            );
+        }
+        minigame.score = 0;
+        minigame.init(logicGrid, setUpdateGrid, setLogicGrid);
+        setLogicGrid(logicGrid);
+        setUpdateGrid(true);
+    }, [reset]);
+
+    let [updateGrid, setUpdateGrid] = useState(false);
+    let [logicGridToUpdate, setLogicGrid] = useState(logicGrid);
     //@ts-ignore
-    return () => clearInterval(myInterval);
-  }, [updateGrid]);
-  console.log(minigame.score);
-  return (
-    <>
-      {minigame && logicGridToUpdate.size ? (
+    useEffect(() => {
+        //@ts-ignore
+        const myInterval = setInterval(() => {
+            minigame.evolve();
+        }, minigame.refreshInterval);
+        if (updateGrid) {
+            if (minigame.score >= minigame.scoreTresh) {
+                localStorage.setItem(
+                    "score",
+                    (
+                        parseInt(localStorage.getItem("score") as string) + minigame.score
+                    ).toString()
+                );
+                router.push("/game").then(() => null);
+            }
+            minigame.update(logicGridToUpdate);
+            setUpdateGrid(false);
+        }
+        //@ts-ignore
+        return () => clearInterval(myInterval);
+    }, [updateGrid]);
+    console.log(minigame.score);
+    return (
         <>
-          <Box id="Canvas">
-            <Box id="Grid">{minigame.ViewGrid(logicGridToUpdate)}</Box>
-          </Box>
-          {minigame.hasLeftRightInputs ? (
-            <>
-              <IconButton
-                aria-label="left"
-                icon={<ArrowLeftIcon />}
-                onClick={() => minigame.playerInput("left", logicGrid)}
-              />
-              <IconButton
-                aria-label="right"
-                icon={<ArrowRightIcon />}
-                onClick={() => minigame.playerInput("right", logicGrid)}
-              />{" "}
-            </>
-          ) : (
-            <></>
-          )}
+            {minigame && logicGridToUpdate.size ? (
+                <>
+                    <Box id="Canvas">
+                        <Box id="Grid">{minigame.ViewGrid(logicGridToUpdate)}</Box>
+                    </Box>
+                    {minigame.Controls()}
+                </>
+            ) : (
+                <></>
+            )}
         </>
-      ) : (
-        <></>
-      )}
-    </>
-  );
+    );
 }
