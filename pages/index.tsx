ec10f00771590bb08582bfe366acf5328283001c
Paul Delifer <paul.delifer@yahoo.fr>
2022-12-09 14:26:20 +0100

    accueuil responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
 import {
-    Box,
-    Button,
-    Center,
-    IconButton,
-    SimpleGrid,
-    Text,
-    keyframes,
+  Box,
+  Button,
+  Center,
+  IconButton,
+  SimpleGrid,
+  Text,
+  keyframes,
 } from "@chakra-ui/react";
 import Link from "next/link";
-import {PlayButton} from "../components/icons/playButton.svg";
-import {breakpoints} from "../theme";
-import {LeaderBoardButton} from "../components/icons/leaderboardButton.svg";
+import { PlayButton } from "../components/icons/playButton.svg";
+import { breakpoints } from "../theme";
+import { LeaderBoardButton } from "../components/icons/leaderboardButton.svg";
 
 const glitch = keyframes`
   0% {
@@ -50,116 +50,129 @@ const glitch = keyframes`
 `;
 
 export default function Home() {
-    const glitchAnimation = `${glitch} 2s infinite linear alternate-reverse`;
-    return (
-        <Center>
-            <Box height="60vh">
-                <SimpleGrid columns={1} spacing="5px" marginTop="20vh">
-                    <Link href="/game">
-                        <Box
-                            sx={{
-                                position: "relative",
-                            }}
-                        >
-                            <IconButton
-                                sx={{
-                                    position: "absolute",
-                                    top: "50vh",
-                                    left: "50%",
-                                    zIndex: 1,
-                                    background: "none",
-                                    display: "flex",
-                                    justifyContent: "center",
-                                    alignItems: "center",
-                                    flexDirection: "column",
-                                }}
-                                icon={
-                                    <PlayButton
-                                        boxSize={breakpoints.playButtonBoxSize}
-                                    ></PlayButton>
-                                }
-                                aria-label={"Button"}
-                            ></IconButton>
-                            <Text
-                                sx={{
-                                    position: "absolute",
-                                    top: "50vh",
-                                    left: "50%",
-                                    zIndex: 1,
-                                    color: "white",
-                                }}
-                                fontSize={breakpoints.fontSize.h3}
-                            >
-                                Play
-                            </Text>
-                        </Box>
-                    </Link>
-                    <Link href="/leaderBoard">
-                        <Box
-                            sx={{
-                                position: "relative",
-                            }}
-                        >
-                            <IconButton
-                                sx={{
-                                    position: "absolute",
-                                    right: "65%",
-                                    zIndex: 1,
-                                    top: "50vh",
-                                    background: "none",
-                                    display: "flex",
-                                    justifyContent: "center",
-                                    alignItems: "center",
-                                    flexDirection: "column",
-                                }}
-                                icon={
-                                    <LeaderBoardButton
-                                        boxSize={breakpoints.playButtonBoxSize}
-                                    ></LeaderBoardButton>
-                                }
-                                aria-label={"Button"}
-                            ></IconButton>
-                            <Text
-                                sx={{
-                                    position: "absolute",
-                                    zIndex: 1,
-                                    color: "white",
-                                    top: "50vh",
-                                    //media query for screen abote 425px
-
-                                }}
-                                fontSize={breakpoints.fontSize.h4}
-                            >
-                                Highscore
-                            </Text>
-                        </Box>
-                    </Link>
-                    <Text
-                        animation={glitchAnimation}
-                        fontSize={breakpoints.fontSize.h1}
-                        sx={{
-                            color: "white",
-                            fontSize: "100px",
-                            position: "relative",
-                            margin: "0 auto",
-                            "&:after": {
-                                content: "attr(data-text)",
-                                position: "absolute",
-                                left: "2px",
-                                textShadow: "-1px 0 red",
-                                top: "0",
-                                color: "white",
-                                background: "black",
-                                overflow: "hidden",
-                                clip: "rect(0,900px,0,0)",
-                                animation: `${glitchAnimation} 0.5s infinite linear alternate-reverse`,
-                            },
-                        }}
-                    >
-                        OverTale
-                    </Text>
-                </SimpleGrid>
+  const glitchAnimation = `${glitch} 2s infinite linear alternate-reverse`;
+  return (
+    <Center>
+      <Box height="60vh">
+        <SimpleGrid columns={1} spacing="5px" marginTop="20vh">
+          <Link href="/game">
+            <Box
+              sx={{
+                position: "relative",
+              }}
+            >
+              <IconButton
+                sx={{
+                  position: "absolute",
+                  top: "50vh",
+                  left: "50%",
+                  zIndex: 1,
+                  background: "none",
+                }}
+                icon={
+                  <PlayButton
+                    boxSize={breakpoints.playButtonBoxSize}
+                  ></PlayButton>
+                }
+                aria-label={"Button"}
+              ></IconButton>
+              <Text
+                sx={{
+                  position: "absolute",
+                  top: "50vh",
+                  left: "60%",
+                  zIndex: 1,
+                  color: "white",
+                }}
+                fontSize={breakpoints.fontSize.h3}
+              >
+                Play
+              </Text>
             </Box>
-        </Center>
-    );
+          </Link>
+          <Link href="/leaderBoard">
+            <Box
+              sx={{
+                position: "relative",
+              }}
+            >
+              <IconButton
+                sx={{
+                  position: "absolute",
+                  right: "65%",
+                  zIndex: 1,
+                  top: "50vh",
+                  background: "none",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "column",
+                }}
+                icon={
+                  <LeaderBoardButton
+                    boxSize={breakpoints.playButtonBoxSize}
+                  ></LeaderBoardButton>
+                }
+                aria-label={"Button"}
+              ></IconButton>
+              <Text
+                sx={{
+                  position: "absolute",
+                  zIndex: 1,
+                  color: "white",
+                  top: "50vh",
+                  left: "8%",
+                  //media query for screen abote 425px
+                  "@media screen and (min-width: 425px)": {
+                    left: "8%",
+                  },
+                  //media query for screen abote 768px
+                  "@media screen and (min-width: 768px)": {
+                    left: "-1%",
+                  },
+                  //media query for screen above 1024px
+                  "@media screen and (min-width: 1024px)": {
+                    left: "-12%",
+                  },
+                  "@media screen and (min-width: 1440px)": {
+                    left: "10%",
+                  },
+                  "@media screen and (min-width: 2560px)": {
+                    left: "10%",
+                  },
+                }}
+                fontSize={breakpoints.fontSize.h4}
+              >
+                Highscore
+              </Text>
+            </Box>
+          </Link>
+          <Text
+            animation={glitchAnimation}
+            fontSize={breakpoints.fontSize.h1}
+            sx={{
+              color: "white",
+              fontSize: "100px",
+              position: "relative",
+              margin: "0 auto",
+              "&:after": {
+                content: "attr(data-text)",
+                position: "absolute",
+                left: "2px",
+                textShadow: "-1px 0 red",
+                top: "0",
+                color: "white",
+                background: "black",
+                overflow: "hidden",
+                clip: "rect(0,900px,0,0)",
+                animation: `${glitchAnimation} 0.5s infinite linear alternate-reverse`,
+              },
+            }}
+          >
+            OverTale
+          </Text>
+        </SimpleGrid>
+      </Box>
+    </Center>
+  );
 }
--- theme.js
+++ theme.js
@@ -1,12 +1,12 @@
 export var breakpoints = {
   // Breakpoints for the different screen sizes
   fontSize: {
-    h1: { base: "24px", md: "40px", lg: "56px" },
-    h2: { base: "20px", md: "32px", lg: "48px" },
-    h3: { base: "18px", md: "28px", lg: "40px" },
-    h4: { base: "16px", md: "24px", lg: "32px" },
-    h5: { base: "14px", md: "20px", lg: "24px" },
-    h6: { base: "12px", md: "16px", lg: "20px" },
+    h1: {base: "24px", md: "40px", lg: "56px"},
+    h2: {base: "20px", md: "32px", lg: "48px"},
+    h3: {base: "18px", md: "28px", lg: "40px"},
+    h4: {base: "16px", md: "24px", lg: "32px"},
+    h5: {base: "14px", md: "20px", lg: "24px"},
+    h6: {base: "12px", md: "16px", lg: "20px"},
   },
   eventCardSize: {
     base: "275px",
@@ -24,6 +24,7 @@ export var breakpoints = {
     xl: 300,
     "2xl": 400,
   },
+  playButtonTextPosition: {}
 };
 
 export var colors = {
