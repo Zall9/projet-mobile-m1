d4e7dd8ea8b952cb1d951ea761cfaa50078f23c5
Paul Delifer <paul.delifer@yahoo.fr>
2022-12-10 18:58:27 +0100

    config for build

--- next.config.js
+++ next.config.js
@@ -1,23 +1,30 @@
 /** @type {import('next').NextConfig} */
 var authDomain = require("./model/ServerSideDB/config.json").authDomain
 const runtimeCaching = require("next-pwa/cache")
-const withPWA = require("next-pwa")({
-    dest: "public",
-    register:true,
-    skipWaiting:true,
-    runtimeCaching,
-    buildExcludes: [/middleware-manifest.json$/]
-});
+const withPWA = require("next-pwa")
+// const withPWA = require("next-pwa")({
+//     dest: "public",
+//     register:true,
+//     skipWaiting:true,
+//     runtimeCaching,
+//     buildExcludes: [/middleware-manifest.json$/]
+// });
 const NextConfig = {
     images: {
         domains: [authDomain],
     },
 };
 module.exports = {
-    ...withPWA,
     ...NextConfig,
     webpack: (config) => {
         config.resolve.fallback = {...config.resolve.fallback, fs: false};
         return config;
     },
+    pwa:{
+        dest: "public",
+        register: true,
+        skipWaiting: true,
+        runtimeCaching,
+        buildExcludes: [/middleware-manifest.json$/]
+    }
 };
