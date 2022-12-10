ad90d70525bb9f2602861aee7199ae90e03e284f
romain22222 <romrom22222@hotmail.fr>
2022-12-10 16:10:40 +0100

    microfix

--- pages/LeaderboardPage.tsx
+++ pages/LeaderboardPage.tsx
@@ -1,43 +1,40 @@
 import React, { useEffect, useState } from "react";
-import {Grid, GridItem, Text} from "@chakra-ui/react";
-import {Leaderboard, ScoreRow} from "../model/Leaderboard";
+import { Grid, GridItem, Text } from "@chakra-ui/react";
+import { Leaderboard, ScoreRow } from "../model/Leaderboard";
 
 const LeaderboardPage = () => {
   const ldb = new Leaderboard();
 
-  console.log('LDB',ldb);
-  const [page, setPage] = useState(0);
-    const [datas,setDatas] = useState<ScoreRow[]>();
+  const [page, setPage] = useState(1);
+  const [datas, setDatas] = useState<ScoreRow[]>();
   useEffect(() => {
-    ldb.init().then(()=>ldb.init().then(() => setDatas(ldb.getLeaderboard(page)))
-    );
-    console.log('datas',datas);
+    ldb.init().then(() => {
+      setDatas(ldb.getLeaderboard(page));
+    });
   }, []);
 
-
   // @ts-ignore
-    return (
+  return (
     <Grid templateColumns={"repeat(6,1fr)"}>
-        {/*@ts-ignore*/}
+      {/*@ts-ignore*/}
 
-        { datas?.map((data) => {
-            return (
-                <>
-                <GridItem colSpan={1}>
-                    <Text>{data.username}</Text>
-                </GridItem>
-                <GridItem colSpan={1}>
-                    <Text>{data.score}</Text>
-                </GridItem>
-                <GridItem colSpan={1}>
-                    <Text>{data.classId}</Text>
-                </GridItem>
-                </>
-            );
-        }) ?? <Text color={"#fbfbfb"}>Chargement</Text>}
+      {datas?.map((data) => {
+        return (
+          <>
+            <GridItem colSpan={1}>
+              <Text>{data.username}</Text>
+            </GridItem>
+            <GridItem colSpan={1}>
+              <Text>{data.score}</Text>
+            </GridItem>
+            <GridItem colSpan={1}>
+              <Text>{data.classId}</Text>
+            </GridItem>
+          </>
+        );
+      }) ?? <Text color={"#fbfbfb"}>Chargement</Text>}
     </Grid>
-    );
+  );
 };
-
 
 export default LeaderboardPage;
