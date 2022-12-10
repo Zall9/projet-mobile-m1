ef15455005716bc5b39ba8883e4dfc92da253fc6
Paul Delifer <paul.delifer@yahoo.fr>
2022-12-10 20:24:59 +0100

    firebase

--- model/ServerSideDB/ServerSideDB.ts
+++ model/ServerSideDB/ServerSideDB.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "@firebase/app";
 import { getDatabase } from "@firebase/database";
+import {getAnalytics} from "@firebase/analytics";
 
 const firebase = require("/model/ServerSideDB/config.json");
 
@@ -18,3 +19,19 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Realtime Database and get a reference to the service
 export const database = getDatabase(app);
+
+const firebaseConfigFront={
+  apiKey: "AIzaSyBXtWY5qQlUvTKzD2AFY4q7l67mGzMAbgQ",
+  authDomain: "hairbook-1605515794372.firebaseapp.com",
+  projectId: "hairbook-1605515794372",
+  storageBucket: "hairbook-1605515794372.appspot.com",
+  messagingSenderId: "842670915786",
+  appId: "1:842670915786:web:e8711db5387019a1b67433",
+  measurementId: "G-5F0LR7JYQM"
+}
+
+//app front is a wep app
+const appFront = initializeApp(firebaseConfigFront);
+const analytics = getAnalytics(appFront);
+
+
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { database } from "../model/ServerSideDB/ServerSideDB";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+
   if (database.type) {
     return (
       <>
