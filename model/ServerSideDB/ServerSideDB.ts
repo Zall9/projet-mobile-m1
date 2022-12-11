774c3507e365866cdbe2ef44fff6e71f180376d5
Paul Delifer <paul.delifer@yahoo.fr>
2022-12-11 12:57:16 +0100

    firebase

--- model/ServerSideDB/ServerSideDB.ts
+++ model/ServerSideDB/ServerSideDB.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "@firebase/app";
 import { getDatabase } from "@firebase/database";
-import {getAnalytics} from "@firebase/analytics";
+import { getAnalytics } from "@firebase/analytics";
 
 const firebase = require("/model/ServerSideDB/config.json");
 
@@ -19,19 +19,17 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Realtime Database and get a reference to the service
 export const database = getDatabase(app);
-
-const firebaseConfigFront={
-  apiKey: "AIzaSyDK0dJw7_eizeBdsopEKf-IWPtlqjRKH-E",
-  authDomain: "projet-mobile-m1-22222.firebaseapp.com",
-  databaseURL: "https://projet-mobile-m1-22222-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "projet-mobile-m1-22222",
-  storageBucket: "projet-mobile-m1-22222.appspot.com",
-  messagingSenderId: "95370679054",
-  appId: "1:95370679054:web:8c0496c1560d84e40e7d5f"
-}
+//
+// const firebaseConfigFront={
+//   apiKey: "AIzaSyDK0dJw7_eizeBdsopEKf-IWPtlqjRKH-E",
+//   authDomain: "projet-mobile-m1-22222.firebaseapp.com",
+//   databaseURL: "https://projet-mobile-m1-22222-default-rtdb.europe-west1.firebasedatabase.app",
+//   projectId: "projet-mobile-m1-22222",
+//   storageBucket: "projet-mobile-m1-22222.appspot.com",
+//   messagingSenderId: "95370679054",
+//   appId: "1:95370679054:web:8c0496c1560d84e40e7d5f"
+// }
 
 //app front is a wep app
-const appFront = initializeApp(firebaseConfigFront, "appFront" );
+// const appFront = initializeApp(firebaseConfigFront, "appFront" );
 //const analytics = getAnalytics(appFront);
-
-
