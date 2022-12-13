d1c845255b44933f3a427dfd6fcdb9f6f1fefe19
romain22222 <romrom22222@hotmail.fr>
2022-12-13 09:29:56 +0100

    fix weird mg

--- model/Minigames/MinigameController.ts
+++ model/Minigames/MinigameController.ts
@@ -4,7 +4,7 @@ let minigameIdList: string[];
 
 export class MinigameController {
   static init() {
-    minigameIdList = ["Memory", "Panier", "RPS", "Unknown"];
+    minigameIdList = ["Memory", "Panier", "RPS"];
   }
 
   static getById(id: string): IMinigame {
@@ -16,9 +16,8 @@ export class MinigameController {
   }
 
   static pickRandomMinigame(): IMinigame {
-    const validMinigames = minigameIdList.filter((v) => v !== "Unknown");
     return this.getById(
-      validMinigames[Math.floor(Math.random() * validMinigames.length)]
+      minigameIdList[Math.floor(Math.random() * minigameIdList.length)]
     );
   }
 }
